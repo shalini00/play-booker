e85449d7457a5014ee74b0d02e0391aa5949042d
Shalini Singh <shalini@Shalinis-MacBook-Air.local>
2025-04-29 00:18:36 +0530

    Initial Commit

---
+++ playbooker/src/main/java/com/app/playbooker/PlaybookerApplication.java
@@ -0,0 +1,13 @@
+package com.app.playbooker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaybookerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlaybookerApplication.class, args);
+	}
+
+}
---
+++ playbooker/src/test/java/com/app/playbooker/PlaybookerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.playbooker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaybookerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
